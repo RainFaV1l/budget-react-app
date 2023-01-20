843d8e959ba2a2c6676f6ee4da3ae66e251ac63a
Azat <Nazmiev_Flesh_Royal@mail.ru>
2023-01-20 14:57:37 +0300

    Add HTML trmplate

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,87 @@
 const HomePage = () => {
 
     return (
+
         <section>
+
             <div className="container">
 
-                <h2>News</h2>
+                <div className="balance">
 
-                <div className="articles">
-                    <h4>Articles..</h4>
+                    <h2>30 000 руб.</h2>
+
+                </div>
+
+                <div className="balance-form">
+
+                    <form>
+
+                        <h3>Добавить операцию</h3>
+
+                        <div className="wrapper">
+
+                            <input 
+                            type="number" 
+                            name="balance" 
+                            placeholder="30 300"
+                            />
+
+                            <select name="category">
+
+                                <option value="products">Продукты</option>
+
+                            </select>
+
+                            <button className="button">Добавить операцию</button>
+
+                        </div>
+
+                    </form>
+
+                </div>
+
+                <div className="operations__wrapper">
+
+                    <h2 className="operation__title">Операции</h2>
+
+                    <div className="filter">
+
+                        <button>Все операции</button>
+                        <button>Все доходы</button>
+                        <button>Все расходы</button>
+
+                    </div>
+
+                    <div className="operations">
+
+                        <div className="operation">
+
+                            <h4 className="total">30 00 руб.</h4>
+
+                            <p className="category">Категория: автомобиль</p>
+
+                            <button className="button button--remove">Удалить</button>
+
+                        </div>
+
+                    </div>
+
+                    <div className="pagination">
+
+                        <button className="pagination__button">
+                        
+                        </button>
+
+                    </div>
+
                 </div>
 
             </div>
+
         </section>
+
     );
-    
+
 }
 
 export default HomePage;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import Root from "../pages/Root";
-import UserPage from "../pages/UserPage";
 
 const router = createBrowserRouter([
 
@@ -12,10 +11,6 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: <HomePage />
-            },
-            {
-                path: "/users/:id",
-                element: <UserPage />
             },
         ]
     },
